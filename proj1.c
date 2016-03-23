630f01d699fd74e03cf465774f8d3186a49967d8
David Coimbra <david.coimbra@tecnico.ulisboa.pt>
2016-03-23 16:58:13 +0000

    COIMBRA MAU!

--- proj1.c
+++ proj1.c
@@ -16,8 +16,10 @@
 typedef struct
 {
 	char id[IDLEN];
-	int capacidade, estado;   /* Aeroporto caraterizado por um um codigo de identificacao de 4 letras, */
-} aeroporto;			      /* capacidade maxima de voos e um estado 0 (encerrado) ou 1 (aberto)     */
+	int capacidade, estado;   /* Aeroporto caraterizado por um codigo de identificacao de 3 letras, */
+	int incoming, outgoing;   /* capacidade maxima de voos e um estado: 0 (encerrado) ou 1 (aberto).*/
+	} aeroporto;			  /* o n. de voos total que sai e chega a um aeroporto tambem esta associado ao mesmo. */
+
 
 /* Prototipo - programa principal */
 
@@ -144,6 +146,9 @@ void adicionaAeroporto(aeroporto vet_aeroportos[])
 	scanf("%s %d", aero.id, &aero.capacidade); /* operacoes de leitura do comando A: codigo de identificacao e capacidade maxima */
 
 	aero.estado = ABERTO;                       /* o aeroporto, quando criado, e' automaticamente aberto */
+	
+	aero.incoming = 0;
+	aero.outgoing = 0;
 
 	vet_aeroportos[i++] = aero;                /* o indice e' incrementado para que o proximo aeroporto seja associado a essa    */
 }											   /*  posicao na proxima chamada da funcao  */
@@ -158,12 +163,18 @@ void alteraCapacidadeMaxima(aeroporto vet_aeroportos[], int numeroAeroportos)
 	scanf("%s %d", aero_id, &aumento_capacidade);
 
 	i = indiceAeroporto(vet_aeroportos, numeroAeroportos, aero_id);
-
-	if ((i != -1) && (vet_aeroportos[i].estado == ABERTO))
+	
+	if (i != -1) 
 	{
-		vet_aeroportos[i].capacidade += aumento_capacidade; /* A capacidade so e alterada caso o aeroporto exista e esteja aberto */ 
+		int total_voos = vet_aeroportos[i].incoming + vet_aeroportos[i].outgoing;
+		int nova_capacidade = vet_aeroportos[i].capacidade + aumento_capacidade;
+		
+		if ((vet_aeroportos[i].estado == ABERTO) && (nova_capacidade >= total_voos))
+		{
+			vet_aeroportos[i].capacidade = nova_capacidade; /* A capacidade so e alterada caso o aeroporto exista, esteja aberto */ 
+		}												/* e caso a nova capacidade seja maior ou igual ao numero de voos do mesmo */
 	}
-
+	
 	else
 	{
 		printf("*Capacidade de %s inalterada\n", aero_id);
