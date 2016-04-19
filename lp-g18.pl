e74e37b87c31f93f1c39daf4b71210da627d565c
David Coimbra <david.coimbra@tecnico.ulisboa.pt>
2016-04-19 16:57:03 +0100

    Predicado auxiliar membro/2 que vai ser útil para verificar se um elemento existe numa lista.

--- lp-g18.pl
+++ lp-g18.pl
@@ -1,12 +1,21 @@
-%                  Grupo 18
-% 57842 Filipa Marques - 84708 David Coimbra
+/*                  Grupo 18                  */
+/* 57842 Filipa Marques - 84708 David Coimbra */
 
-% movs_possiveis(Lab, Pos_atual, Movs, Poss).
+/* Programa principal */
 
-distancia((L1, C1), (L2, C2), Dist) :- Dist is (abs(L1 - L2) + abs(C1 - C2)).
+% movs_possiveis(Lab, Pos_atual, Movs, Poss). 
+
+% distancia/3: calcula Dist como a distancia entre (L1, C1) e (L2, C2).
+distancia((L1, C1), (L2, C2), Dist) :- Dist is (abs(L1-L2) + abs(C1-C2)).
 
 % ordena_poss(Poss, Poss_ord, Pos_inicial, Pos_final).
 
 % resolve1(Lab, Pos_inicial, Pos_final, Movs).
 
 % resolve2(Lab, Pos_inicial, Pos_final, Movs).
+
+/* Predicados auxiliares */
+
+% membro/2: verifica se o elemento Elem faz parte da lista dada.
+membro(Elem, [Elem | _]).
+membro(Elem, [_ | Cauda]) :- membro(Elem, Cauda).
