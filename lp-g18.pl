96d081aff0da92e04924903868d9581208ca39b6
David Coimbra <david.coimbra@tecnico.ulisboa.pt>
2016-04-19 15:58:34 +0100

    Predicado distancia/3

--- lp-g18.pl
+++ lp-g18.pl
@@ -1,1 +1,12 @@
-% 57842 - Filipa Marques - Grupo 18 - 84708 - David Coimbra %
+%                  Grupo 18
+% 57842 Filipa Marques - 84708 David Coimbra
+
+% movs_possiveis(Lab, Pos_atual, Movs, Poss).
+
+distancia((L1, C1), (L2, C2), Dist) :- Dist is (abs(L1 - L2) + abs(C1 - C2)).
+
+% ordena_poss(Poss, Poss_ord, Pos_inicial, Pos_final).
+
+% resolve1(Lab, Pos_inicial, Pos_final, Movs).
+
+% resolve2(Lab, Pos_inicial, Pos_final, Movs).
