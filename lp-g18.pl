ebcf5a13676c1ee4195e59b159a4dbd0a34da0d8
David Coimbra <david.coimbra@tecnico.ulisboa.pt>
2016-04-21 14:18:14 +0100

    Substituí os not por \+, tirei os parentesis a mais na nao_pertence (só os que é seguro remover) e adicionei parentesis nas disjunçoes da seleciona_se_nao_pertence e seleciona_se_sem_paredes para agrupar melhor a info.

--- lp-g18.pl
+++ lp-g18.pl
@@ -20,7 +20,7 @@ distancia((L1, C1), (L2, C2), Dist) :- Dist is (abs(L1 - L2) + abs(C1 - C2)).
 
 % membro/2: verifica se o elemento Elem faz parte da lista dada.
 % membro(Elem, [Elem|_]).    % caso de paragem
-% membro(Elem, [_|Cauda]) :- membro(Elem, Cauda).
+% membro(Elem, [_|Cauda]) :- membro(Elem, Cauda
 
 % pos_i/3: determina o valor da lista no indice I dado.
 pos_i([Cabeca|_], 1, Cabeca). % caso de paragem
@@ -35,18 +35,18 @@ adjacentes((L, C), [AdjH|AdjT]) :- Lcima is L-1, Lbaixo is L+1, Cesq is C-1, Cdi
 
 % nao_pertence/2: verdadeiro se um certo movimento nao pertencer a lista de movimentos dada
 nao_pertence(_, []). % caso de paragem 
-nao_pertence((_, L, C), [(_, Linha, Coluna)|MovsT]) :- ((L \= Linha) ; (C \= Coluna) ),
-													  nao_pertence((_, L, C), MovsT).
+nao_pertence((_, L, C), [(_, Linha, Coluna)|MovsT]) :- (L \== Linha ; C \== Coluna),
+													   nao_pertence((_, L, C), MovsT).
 													  
 %seleciona_se_nao_pertence/3: verdadeiro se Res corresponder a todos os movimentos de Adj cujas posicoes nao pertencem a Movs
 seleciona_se_nao_pertence([AdjH|AdjT], Movs, Res) :- seleciona_se_nao_pertence([AdjH|AdjT], Movs, Res, []).
 seleciona_se_nao_pertence([],_, Acc, Acc).
-seleciona_se_nao_pertence([AdjH|AdjT], Movs, Res, Acc) :- nao_pertence(AdjH, Movs),
-													   concatena(Acc, [AdjH], Acc1),
-													   seleciona_se_nao_pertence(AdjT, Movs, Res, Acc1);
-													   % ou:
-													   not(nao_pertence(AdjH, Movs)),
-													   seleciona_se_nao_pertence(AdjT, Movs, Res, Acc).
+seleciona_se_nao_pertence([AdjH|AdjT], Movs, Res, Acc) :- (nao_pertence(AdjH, Movs),
+													      concatena(Acc, [AdjH], Acc1),
+													      seleciona_se_nao_pertence(AdjT, Movs, Res, Acc1));
+													      % ou:
+													      (\+(nao_pertence(AdjH, Movs)),
+													      seleciona_se_nao_pertence(AdjT, Movs, Res, Acc)).
 
 % concatena/3: corresponde a concatenação das Listas 1 e 2.
 concatena([], NovaCauda, NovaCauda).
@@ -60,17 +60,16 @@ celula_labirinto(Lab, (L, C), Celula) :- pos_i(Lab, L, Linha), pos_i(Linha, C, C
 
 % sem_paredes/2: verdadeiro se não existir uma parede na direção do movimento dado.
 sem_paredes(_, []).
-sem_paredes((D,_,_), [CelH|CelT]) :- CelH \= D, sem_paredes((D,_,_), CelT).
+sem_paredes((D,_,_), [CelH|CelT]) :- CelH \== D, sem_paredes((D,_,_), CelT).
 
 %seleciona_se_sem_paredes/3: verdadeiro se Res corresponder a todos os movimentos de MovsPoss cuja direcao nao esta bloqueada por paredes.
 seleciona_se_sem_paredes(Paredes, MovsPoss, Res) :- seleciona_se_sem_paredes(Paredes, MovsPoss, Res, []).
 seleciona_se_sem_paredes(_, [], Acc, Acc).
-seleciona_se_sem_paredes(Paredes, [MovsPossH|MovsPossT], Res, Acc) :- sem_paredes(MovsPossH, Paredes),
+seleciona_se_sem_paredes(Paredes, [MovsPossH|MovsPossT], Res, Acc) :- (sem_paredes(MovsPossH, Paredes),
 																	  concatena(Acc, [MovsPossH], Acc1),
-																	  seleciona_se_sem_paredes(Paredes, MovsPossT, Res, Acc1);
+																	  seleciona_se_sem_paredes(Paredes, MovsPossT, Res, Acc1));
 																	  % ou:
-																	  not(sem_paredes(MovsPossH, Paredes)),
-																	  seleciona_se_sem_paredes(Paredes, MovsPossT, Res, Acc).
+																	  (\+(sem_paredes(MovsPossH, Paredes)),
+																	  seleciona_se_sem_paredes(Paredes, MovsPossT, Res, Acc)).
 
 % [[[d,e,c],[e,b,c],[b,c],[c],[d,c],[d,e,c]], [[e,b],[b,c],[b,c],[],[b],[d,b]], [[e,c],[b,c],[b,c],[b],[b,c],[d,b,c]], [[d,e],[e,c],[c],[c],[c],[d,c]], [[e,b],[d,b],[e,b],[b],[b],[d,b]]]
-
