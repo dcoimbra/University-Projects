ecd048f67734b4a8e4b4e0fa60edb332964d8653
filipaBit <filipalpgouveia@gmail.com>
2016-04-22 01:45:30 +0100

    *Modifiquei todas as funções auxiliares da ordenação para devolverem só true ou false.        - Desta forma em vez da primeiro_elem temos uma funçãi "e_prioritario" que devolve true de o 1M1 for prioritario ao M2. *Criei a função "insere_em_lista_ordenada" que insere um X no sitio certo de uma lista já ordenada. *a Ordena_Poss, basicamente vai pegar na H da lista e inseri-la no sitio certo da lista que, vai "ela propria" criando (ela propria, meaning, é na verdade a insere_em_lista_ordenada que vai construindo a lista, a partir dos elementos que a ordena lhe vai passando)
    *modifiquei a estética dos comentários para não ficarem eeeeeenormes e a sair da janela
    
    ...acho que é tudo... testa isto com os testes da prof (parece-me estar a funcionar, mas quem sabe!)

--- lp-g18.pl
+++ lp-g18.pl
@@ -3,7 +3,8 @@
 
 /* Programa principal */
 
-% movs_possiveis/4: dados um labirinto Lab, uma posicao Pos_atual e os movimentos ja efetuados Movs, os movimentos possiveis sao Poss.
+% movs_possiveis/4: dados um labirinto Lab, uma posicao Pos_atual e os movimentos ja efetuados Movs,
+% os movimentos possiveis sao Poss.
 movs_possiveis(Lab, Pos_actual, Movs, Poss) :- 
 	adjacentes(Pos_actual, Adj), celula_labirinto(Lab, Pos_actual, Paredes), 
 	seleciona_se_sem_paredes(Paredes, Adj, PossAux),
@@ -13,8 +14,12 @@ movs_possiveis(Lab, Pos_actual, Movs, Poss) :-
 distancia((L1, C1), (L2, C2), Dist) :- 
 	Dist is (abs(L1 - L2) + abs(C1 - C2)).
 
-% ordena_poss(Poss, Poss_ord, Pos_inicial, Pos_final).
-
+% ordena_poss/4:.
+ordena_poss([], [], _, _).
+ordena_poss([PossH|PossT], Poss_ord, Pos_inicial, Pos_final):-
+	ordena_poss(PossT, Poss_ordAux, Pos_inicial, Pos_final),
+	insere_em_lista_ordenada(Poss_ordAux, PossH, Poss_ord, Pos_inicial, Pos_final).
+	
 % resolve1(Lab, Pos_inicial, Pos_final, Movs).
 
 % resolve2(Lab, Pos_inicial, Pos_final, Movs).
@@ -29,19 +34,20 @@ pos_i([Cabeca|Cauda], I, Elemento) :-
 	IAux is (I - 1),
 	pos_i(Cauda, IAux, Elemento).
 
-% adjacentes/2: verdadeiro se a lista Adj for uma lista de pontos adjacentes ao ponto (L, C)
+% adjacentes/2: verdadeiro se a lista Adj for uma lista de pontos adjacentes ao ponto (L, C).
 adjacentes((L, C), Adj) :- 
 	Lcima is L-1, Lbaixo is L+1, Cesq is C-1, Cdir is C+1,
 	Adj = [(c, Lcima, C), (b, Lbaixo, C), (e, L, Cesq), (d, L, Cdir)].
 
-% nao_pertence/2: verdadeiro se um certo movimento nao pertencer a lista de movimentos dada
+% nao_pertence/2: verdadeiro se um certo movimento nao pertencer a lista de movimentos dada.
 nao_pertence(_, []). 
 
 nao_pertence((_, L, C), [(_, Linha, Coluna)|MovsT]) :- 
 	(L \== Linha ; C \== Coluna),
 	nao_pertence((_, L, C), MovsT).
-													  
-%seleciona_se_nao_pertence/3: verdadeiro se Res corresponder a todos os movimentos de Adj cujas posicoes nao pertencem a Movs
+
+%seleciona_se_nao_pertence/3: verdadeiro se Res corresponder a todos os 
+%movimentos de Adj cujas posicoes nao pertencem a Movs.
 seleciona_se_nao_pertence([AdjH|AdjT], Movs, Res) :- 
 	seleciona_se_nao_pertence([AdjH|AdjT], Movs, Res, []).
 
@@ -71,7 +77,8 @@ sem_paredes(_, []).
 sem_paredes((D,_,_), [CelH|CelT]) :- 
 	CelH \== D, sem_paredes((D,_,_), CelT).
 
-%seleciona_se_sem_paredes/3: verdadeiro se Res corresponder a todos os movimentos de MovsPoss cuja direcao nao esta bloqueada por paredes.
+%seleciona_se_sem_paredes/3: verdadeiro se Res corresponder a todos os 
+%movimentos de MovsPoss cuja direcao nao esta bloqueada por paredes.
 seleciona_se_sem_paredes(Paredes, MovsPoss, Res) :- 
 	seleciona_se_sem_paredes(Paredes, MovsPoss, Res, []).
 
@@ -85,38 +92,34 @@ seleciona_se_sem_paredes(Paredes, [MovsPossH|MovsPossT], Res, Acc) :-
 	(\+(sem_paredes(MovsPossH, Paredes)),
 	seleciona_se_sem_paredes(Paredes, MovsPossT, Res, Acc)).
 
-% dist2pontos/5: calcula as distancias de dois movimentos relativamente a uma posicao, em Dist1 e Dist2 respetivamente.
-dist2pontos((L, C), (_, L1, C1), (_, L2, C2), Dist1, Dist2) :-
+% dist2pontos/4: calcula as distancias de dois movimentos relativamente a 
+%uma posicao, e devolve a diferenca das mesmas.
+dist2pontos((L, C), (_, L1, C1), (_, L2, C2), Dist) :-
 	distancia((L1, C1), (L, C), Dist1),
-	distancia((L2, C2), (L, C), Dist2).
+	distancia((L2, C2), (L, C), Dist2),
+	Dist is Dist1-Dist2.
 
-% avalia_letra/4: .
-avalia_letra((D1,L1,C1), (D2,L2,C2), [LetrasH|LetrasT], Eval) :-
-	((D1 == LetrasH, Eval = (D1,L1,C1)), !; (D2 == LetrasH, Eval = (D2,L2,C2)), !); 
-	avalia_letra((D1,L1,C1),(D2,L2,C2), LetrasT, Eval).
+% avalia_letra/3: devolve true caso o mov1 = (D1,L1,C1) seja prioritario ao mov2 = (D2, L2,C2).
+avalia_letra((D1,L1,C1), (D2,L2,C2), [LetrasH|LetrasT]) :-
+	(D1 == LetrasH, !);
+	(D2 \== LetrasH, avalia_letra((D1,L1,C1),(D2,L2,C2), LetrasT)).
 
-% compara_dist/5: .
-compara_dist(Pos_inicial, Pos_final, M1, M2, Res) :-
-   dist2pontos(Pos_final, M1, M2, Dist1, Dist2),
-   ((Dist1 < Dist2, Res = M1);
-    (Dist1 > Dist2, Res = M2);
-   ((Dist1 == Dist2, dist2pontos(Pos_inicial, M1, M2, Dist1_ini, Dist2_ini)),
-
-   ((Dist1_ini > Dist2_ini, Res = M1);
-    (Dist1_ini < Dist2_ini, Res = M2);
-    (Dist1_ini == Dist2_ini, avalia_letra(M1, M2, [c,b,e,d], Res))
-   ) ) ).
-
-%primeiro_elem/4: escreve em X o elemento mais prioritario de uma lista, segundo as regras do predicado 'ordena_poss', descritas no enunciado.
-primeiro_elem([PossH|_], Pos_inicial, Pos_final, X) :-
-	(length([PossH|_], Len), Len < 2, X = PossH).
-
-primeiro_elem([PossH|[PossTH|PossTT]], Pos_inicial, Pos_final, X) :-
-   (
-	( compara_dist(Pos_inicial, Pos_final, PossH, PossTH, Y), Y == PossH, primeiro_elem([PossH|PossTT], Pos_inicial, Pos_final, X) );
-	( compara_dist(Pos_inicial, Pos_final, PossH, PossTH, Y), Y == PossTH, primeiro_elem([PossTH|PossTT], Pos_inicial, Pos_final, X) )
-   ).
-
+%e_prioritario/4: retorna true se M1 for prioriario a M2 e false caso contrario.
+e_prioritario(M1, M2, Pos_inicial, Pos_final) :-
+	dist2pontos(Pos_final, M1, M2, Dist_fin),
+   ( (Dist_fin < 0, !);
+	
+   ( (Dist_fin == 0, dist2pontos(Pos_inicial, M1, M2, Dist_ini)),
+   ( Dist_ini > 0;
+	(Dist_ini == 0, avalia_letra(M1, M2, [c,b,e,d]))
+	) ) ).
+	
+%insere_em_lista_ordenada/5: dadas uma lista, um elemento e um predicado de prioridades,
+%devolve em Res o resultado da insercao do elemento X na lista, na posicao correcta.
+insere_em_lista_ordenada([], X, [X],_,_).
+insere_em_lista_ordenada([H|T], X, Res, Pos_inicial, Pos_final):-
+	(e_prioritario(X, H, Pos_inicial, Pos_final), Res = [X|[H|T]], !);
+	(insere_em_lista_ordenada(T, X, Res1, Pos_inicial, Pos_final), Res = [H|Res1] ).
 
 %[[[d,e,c],[e,b,c],[b,c],[c],[d,c],[d,e,c]], [[e,b],[b,c],[b,c],[],[b],[d,b]], [[e,c],[b,c],[b,c],[b],[b,c],[d,b,c]], [[d,e],[e,c],[c],[c],[c],[d,c]], [[e,b],[d,b],[e,b],[b],[b],[d,b]]].
 
