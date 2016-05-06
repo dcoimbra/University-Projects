1e671d653628f0faedba49e89da9620393dec39a
filipaBit <filipalpgouveia@gmail.com>
2016-05-06 14:57:20 +0100

    todos os outros ficheiros que dá jeito ter por perto: *.c e .h para as operaçoes base das listas * ficheiros .in e .out de teste * .pdf do enunciado e das dicas

---
+++ listas.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "operacoes_base.h"
+
+typedef item;
+
+typedef struct node{
+	item value;
+	struct node*next;
+}*link;
+
+link remove(){
+
+}
