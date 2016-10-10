e1afcf60335e60fe70a0fb26150738b3f8ae0d99
filipaBit <filipalpgouveia@gmail.com>
2016-10-10 12:27:07 +0100

    Input:      - acrescentei mais um SIMULAR e pus SAIR AGORA em vez do SAIR
    i-banco:
         - passei o código de saida para uma função à parte (funcaoSaida)
              * corrigi o bug do i e do j
         - função enviarSignal (para o comando Sair Agora)
         - passei o fork para a main
              * o pai conta o numero de filhos e faz um vector com os seus pid's
    
    contas(.h e .c):
         - acrescentei o protótipo do tratarSignal ao .h
         - handler do signal acrescentado
         - retirado o fork (está no i-banco)

--- contas.c
+++ contas.c
@@ -2,11 +2,12 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
 
 #define atrasar() sleep(ATRASO)
 		     
 int contasSaldos[NUM_CONTAS];
-
+int recebeuSignal = 0;
 
 int contaExiste(int idConta) {
   return (idConta > 0 && idConta <= NUM_CONTAS);
@@ -38,48 +39,58 @@ int creditar(int idConta, int valor) {
 }
 
 int lerSaldo(int idConta) {
-  atrasar();
+  atrasar(); 
   if (!contaExiste(idConta))
     return -1;
   return contasSaldos[idConta - 1];
 }
 
 void simular(int numAnos) {
-  int pid;
 
-  pid = fork();
+  int ano, i, aux, simulSaldo;
+  int auxContasSaldos[NUM_CONTAS];
 
-  if (pid == 0) { 
-    int ano, i, aux, simulSaldo;
-    int auxContasSaldos[NUM_CONTAS];
-
-    for (i = 0; i < NUM_CONTAS; i++) {
-      auxContasSaldos[i] = lerSaldo(i + 1);
-    }
-
-    for (ano = 0; ano <= numAnos; ano++) {
-
-      printf("SIMULACAO: Ano %d\n", ano);
-      printf("=================\n");
-
-      for(i = 0; i < NUM_CONTAS; i++) {
-
-        if(ano == 0)
-          simulSaldo = auxContasSaldos[i];
-
-        else {
-          aux = (auxContasSaldos[i]*(1 + TAXAJURO) - CUSTOMANUTENCAO);
-          simulSaldo = (aux > 0 ? aux : 0);
-          auxContasSaldos[i] = simulSaldo;
-        }
-
-        printf("Conta %d, Saldo %d\n", (i + 1), simulSaldo);
-      }
-      
-      printf("\n");
-    }
-
-    atrasar();
-    exit(0);
+  if (signal(SIGUSR1, tratarSignal) == SIG_ERR) { /* Faz o handle do signal e previne */
+    perror ("Erro.");                  /* caso haja algum problema nessa "instalacao" */
   }
-} 
+
+  for (i = 0; i < NUM_CONTAS; i++) {
+    auxContasSaldos[i] = lerSaldo(i + 1);
+  }
+
+
+  for (ano = 0; ano <= numAnos; ano++) {
+    printf("SIMULACAO: Ano %d\n", ano);
+    printf("=================\n");
+
+    for(i = 0; i < NUM_CONTAS; i++) {
+      atrasar();
+      if (ano == 0)
+        simulSaldo = auxContasSaldos[i];
+
+      else {
+        aux = (auxContasSaldos[i]*(1 + TAXAJURO) - CUSTOMANUTENCAO);
+        simulSaldo = (aux > 0 ? aux : 0);  /* se a simulacao resultar num valor negativo, e' apresentado o valor 0. */
+        auxContasSaldos[i] = simulSaldo;
+      }
+
+      printf("Conta %d, Saldo %d\n", (i + 1), simulSaldo);
+    }
+    
+    printf("\n");
+
+    if (recebeuSignal) {
+    	printf(" --- Simulacao terminada por signal --- \n");
+    	exit(EXIT_FAILURE);
+    }
+  }
+
+
+  exit(EXIT_SUCCESS); /* retorna ao processo pai */
+}
+
+
+void tratarSignal(int signum) {
+
+  recebeuSignal = 1;
+}
--- i-banco.c
+++ i-banco.c
@@ -12,27 +12,31 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+
 #define COMANDO_DEBITAR "debitar"
 #define COMANDO_CREDITAR "creditar"
 #define COMANDO_LER_SALDO "lerSaldo"
 #define COMANDO_SIMULAR "simular"
 #define COMANDO_SAIR "sair"
-#define COMANDO_SAIR_AGORA "sair agora"
+#define COMANDO_SAIR_AGORA "agora"
 
-#define atrasar() sleep(ATRASO)
+
+#define MAX_CHILDREN 20
 
 #define MAXARGS 3
 #define BUFFER_SIZE 100
 
-#define MAX_CHILDREN 20
+void funcaoSaida();
+void enviarSignal();
 
+int pidFilhos[MAX_CHILDREN];
+int nFilhos = 0; /* numero de processos filho criados */
 
 int main (int argc, char** argv) {
 
     char *args[MAXARGS + 1];
     char buffer[BUFFER_SIZE];
-
-    int nFilhos = 0; /* numero de processos filho criados */
+    
 
     inicializarContas();
 
@@ -46,53 +50,15 @@ int main (int argc, char** argv) {
         /* EOF (end of file) do stdin ou comando "sair" */
         if (numargs < 0 ||
 	        (numargs > 0 && (strcmp(args[0], COMANDO_SAIR) == 0))) {
+
+        /* Sair Agora */
+            if ((args[1] != NULL) && (strcmp(args[1], COMANDO_SAIR_AGORA)) == 0) {
+                enviarSignal();
+            }
             
-            int i = 0, j = 0, pid, estado;
- 
-
-            int pids_sucess[MAX_CHILDREN];
-            int pids_failure[MAX_CHILDREN];
-
-            printf("i-banco vai terminar.\n");
-            printf("--\n");
-
-            while ((i+j) < nFilhos) {
-                
-                /* espera pelo fim de cada processo filho */
-                pid = wait(&estado);
-
-                /* Os PID's dos processos filhos 
-                   são guardados no vetor correspondente
-                   ao sucesso desse processo na terminação */
-                
-                if (WIFEXITED(estado)) {
-                    pids_sucess[i] = pid;
-                    i++;
-                }
-                
-                else {
-                    pids_failure[j] = pid;
-                    j++;
-                }
-            }
-
-            while (i) {
-                printf("FILHO TERMINADO (PID=%d; terminou normalmente)\n", pids_sucess[i]);
-                i--;
-            }
-
-            while (j) {
-                printf("FILHO TERMINADO (PID=%d; terminou abruptamente)\n", pids_failure[i]);
-                j--;
-            }
-
-            printf("--\n");
-            printf("i-banco terminou.\n");
-
+            funcaoSaida();
             exit(EXIT_SUCCESS);
         }
-
-        /*sair agora*/
     
         else if (numargs == 0)
             /* Nenhum argumento; ignora e volta a pedir */
@@ -103,72 +69,136 @@ int main (int argc, char** argv) {
             int idConta, valor;
             if (numargs < 3) {
                 printf("%s: Sintaxe inválida, tente de novo.\n", COMANDO_DEBITAR);
-	           continue;
+               continue;
             }
 
             idConta = atoi(args[1]);
             valor = atoi(args[2]);
 
             if (debitar (idConta, valor) < 0)
-	           printf("%s(%d, %d): Erro\n\n", COMANDO_DEBITAR, idConta, valor);
+               printf("%s(%d, %d): Erro\n\n", COMANDO_DEBITAR, idConta, valor);
             else
                 printf("%s(%d, %d): OK\n\n", COMANDO_DEBITAR, idConta, valor);
-    }
-
-    /* Creditar */
-    else if (strcmp(args[0], COMANDO_CREDITAR) == 0) {
-        int idConta, valor;
-        if (numargs < 3) {
-            printf("%s: Sintaxe inválida, tente de novo.\n", COMANDO_CREDITAR);
-            continue;
         }
 
-        idConta = atoi(args[1]);
-        valor = atoi(args[2]);
+        /* Creditar */
+        else if (strcmp(args[0], COMANDO_CREDITAR) == 0) {
+            int idConta, valor;
+            if (numargs < 3) {
+                printf("%s: Sintaxe inválida, tente de novo.\n", COMANDO_CREDITAR);
+                continue;
+            }
 
-        if (creditar (idConta, valor) < 0)
-            printf("%s(%d, %d): Erro\n\n", COMANDO_CREDITAR, idConta, valor);
-        else
-            printf("%s(%d, %d): OK\n\n", COMANDO_CREDITAR, idConta, valor);
-    }
+            idConta = atoi(args[1]);
+            valor = atoi(args[2]);
 
-    /* Ler Saldo */
-    else if (strcmp(args[0], COMANDO_LER_SALDO) == 0) {
-        int idConta, saldo;
-
-        if (numargs < 2) {
-            printf("%s: Sintaxe inválida, tente de novo.\n", COMANDO_LER_SALDO);
-            continue;
+            if (creditar (idConta, valor) < 0)
+                printf("%s(%d, %d): Erro\n\n", COMANDO_CREDITAR, idConta, valor);
+            else
+                printf("%s(%d, %d): OK\n\n", COMANDO_CREDITAR, idConta, valor);
         }
-        idConta = atoi(args[1]);
-        saldo = lerSaldo (idConta);
-        if (saldo < 0)
-            printf("%s(%d): Erro.\n\n", COMANDO_LER_SALDO, idConta);
-        else
-            printf("%s(%d): O saldo da conta é %d.\n\n", COMANDO_LER_SALDO, idConta, saldo);
-    }
 
-    /* Simular */
-    else if (strcmp(args[0], COMANDO_SIMULAR) == 0) {
-        int numAnos;
+        /* Ler Saldo */
+        else if (strcmp(args[0], COMANDO_LER_SALDO) == 0) {
+            int idConta, saldo;
 
-        if (numargs < 2) {
-            printf("%s: Sintaxe inválida, tente de novo.\n", COMANDO_LER_SALDO);
-            continue;
+            if (numargs < 2) {
+                printf("%s: Sintaxe inválida, tente de novo.\n", COMANDO_LER_SALDO);
+                continue;
+            }
+            idConta = atoi(args[1]);
+            saldo = lerSaldo (idConta);
+            if (saldo < 0)
+                printf("%s(%d): Erro.\n\n", COMANDO_LER_SALDO, idConta);
+            else
+                printf("%s(%d): O saldo da conta é %d.\n\n", COMANDO_LER_SALDO, idConta, saldo);
         }
-        numAnos = atoi(args[1]);
-        if (numAnos < 0)
-            printf("%s(%d): Erro.\n\n", COMANDO_SIMULAR, numAnos);
+
+        /* Simular */
+        else if (strcmp(args[0], COMANDO_SIMULAR) == 0) {
+            int numAnos, pid;
+
+            if (numargs < 2) {
+                printf("%s: Sintaxe inválida, tente de novo.\n", COMANDO_SIMULAR);
+                continue;
+            }
+            numAnos = atoi(args[1]);
+
+            if (numAnos < 0)
+                printf("%s(%d): Erro.\n\n", COMANDO_SIMULAR, numAnos);
+
+            else {
+                pid = fork();
+
+            /* Processo Filho */
+                if (pid == 0) {
+                    simular(numAnos);
+                }
+
+            /* Processo Pai */
+                else {
+                    pidFilhos[nFilhos++] = pid;
+                }
+
+            }
+        }
+
         else {
-            simular(numAnos);
-            nFilhos++;
+          printf("Comando desconhecido. Tente de novo.\n");
         }
-    }
 
-    else {
-      printf("Comando desconhecido. Tente de novo.\n");
-    }
-
-  } 
+    } 
 }
 
+
+void funcaoSaida() {
+
+  int i = 0, j = 0, pid, estado; 
+
+  int pids_sucess[MAX_CHILDREN];
+  int pids_failure[MAX_CHILDREN];
+
+  printf("i-banco vai terminar.\n");
+  printf("--\n");
+
+  while ((i+j) < (nFilhos)) { 
+
+      /* espera pelo fim de cada processo filho */
+      pid = wait(&estado);
+    
+      /* Os PID's dos processos filhos 
+         são guardados no vetor correspondente
+         ao sucesso desse processo na terminação */
+      
+      if (!WEXITSTATUS(estado)) {
+          pids_sucess[i] = pid;
+          i++;
+      }
+      
+      else {
+          pids_failure[j] = pid;
+          j++;
+      }
+  }
+
+  while (i != 0) {
+    i--;    /* porque o i (e o j) e' sempre incrementado depois de se meter o pid no vector. */
+    printf("FILHO TERMINADO (PID=%d; terminou normalmente)\n", pids_sucess[i]);
+  }
+
+  while (j != 0) {
+    j--;
+    printf("FILHO TERMINADO (PID=%d; terminou abruptamente)\n", pids_failure[j]);
+  }
+
+  printf("--\n");
+  printf("i-banco terminou.\n");
+}
+
+void enviarSignal() {
+    int i;
+
+    for (i = 0; i < nFilhos; i++) {
+        kill(pidFilhos[i], SIGUSR1);
+    }
+}
