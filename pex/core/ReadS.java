24304f62b29922af3b1d2b23dfcf46f984a55e53
filipaBit <filipalpgouveia@gmail.com>
2016-12-03 20:35:48 +0000

    faltou este

--- pex/core/ReadS.java
+++ pex/core/ReadS.java
@@ -1,11 +1,24 @@
 package pex.core;
+import pex.AppIO;
+import pex.app.App;
 
 public class ReadS extends CompositeExpression {
+	AppIO _appIO;
 
+	public ReadS(AppIO app) {
+		_appIO = app;
+	}
+	
 	@Override
 	public String getAsText() { 
 		
 		return "(reads)";
+	}
+
+	@Override
+	public Literal evaluate() {
+		
+		return new StringLiteral(_appIO.readString());
 	}
 
 }
