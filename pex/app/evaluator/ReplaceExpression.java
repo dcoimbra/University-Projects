cf0bbbd753d72422e12a990afef790a13bd69106
coimbra <david.coimbra@tecnico.ulisboa.pt>
2016-12-03 19:18:23 +0000

    ReplaceExpression a funcionar

--- pex/app/evaluator/ReplaceExpression.java
+++ pex/app/evaluator/ReplaceExpression.java
@@ -5,7 +5,11 @@ package pex.app.evaluator;
 import pex.app.BadExpressionException;
 import pex.app.BadPositionException;
 
+import pex.parser.NewParser;
+import pex.parser.ParserException;
+
 import pex.core.Program;
+import pex.core.Expression;
 
 import pt.utl.ist.po.ui.Form;
 import pt.utl.ist.po.ui.InputString;
@@ -30,6 +34,35 @@ public class ReplaceExpression extends ProgramCommand {
     @Override
     public final void execute() throws BadExpressionException, BadPositionException {
         
-        //FIXME implement
-    }
+        Form f1 = new Form();
+        InputInteger inputPosition = new InputInteger(f1, Message.requestPosition());
+        f1.parse();
+
+        int position = inputPosition.value();
+
+        Form f2 = new Form();
+        InputString inputExpression = new InputString(f2, Message.requestExpression());
+        f2.parse();
+
+        String strExpression = inputExpression.value();
+
+        NewParser parser = new NewParser();
+
+        try {
+
+            Expression expression = parser.parseString(strExpression, entity());
+
+            entity().replace(position, expression);
+        }
+
+        catch (IndexOutOfBoundsException iobe) {
+
+            throw new BadPositionException(position);
+        }
+
+        catch (ParserException pe) {
+
+            throw new BadExpressionException(strExpression);
+        }
+    }   
 }
