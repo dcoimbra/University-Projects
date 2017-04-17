ddfa34be88eb3f28dead6009817930ab58f007ed
catarinacustodio <catarina.custodio@tecnico.ulisboa.pt>
2017-04-17 22:50:30 +0100

    popup no login por impressão digital já funciona!!

--- prototipo/js/functions.js
+++ prototipo/js/functions.js
@@ -1,20 +1,34 @@
 function goBack() {
-    
+
     window.history.back();
 }
 
-                        
+function makeBarInvisible() {
+    var i;
+    var popup = document.getElementsByClassName("popup");
+    for (i = 0; i < popup.length; i++)
+        popup[i].style.visibility='hidden';
+}
+
+function showBar() {
+    var i;
+    var popup = document.getElementsByClassName("popup");
+    for (i = 0; i < popup.length; i++)
+        popup[i].style.visibility='visible';
+    move();
+}
+
 function move() {
-                var elem = document.getElementById("myBar");
-                var width = 0;
-                var id = setInterval(frame, 10);
-                function frame() {
-                    if (width == 100) {
-                        clearInterval(id);
-                        window.location="pessoal.html";
-                    } else {
-                        width++;
-                        elem.style.width = width + '%';
-                    }
-                }
-            }
+    var elem = document.getElementById("myBar");
+    var width = 0;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (width == 100) {
+            clearInterval(id);
+            window.location="pessoal.html";
+        } else {
+            width++;
+            elem.style.width = width + '%';
+        }
+    }
+}
