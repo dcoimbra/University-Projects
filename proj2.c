0e13757d01332e997f35c318647c53e51e811e71
Coimbra <david.coimbra@tecnico.ulisboa.pt>
2017-04-23 13:32:37 +0100

    Encontra uma árvore por Prim e verifica se é insuficiente

--- proj2.c
+++ proj2.c
@@ -1,6 +1,11 @@
 /* Standard library */
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+
+#define NIL -1
+#define TRUE 1
+#define FALSE 0
 
 /* Structures */
 typedef struct node {
@@ -17,11 +22,17 @@ typedef struct graph {
 	Node* adjacency_list;
 }* Graph;
 
+/* Global variables */
+int* key;
+int* pi;
+int* visited;
+
 /* Prototype */
 Graph initGraph(int vertices);
 void addEdge(Graph graph, int origin, int destination, int cost);
 Node addNode(int vertex, int cost, Node head);
-int getGraphCost(Graph graph);
+void prim(Graph g, int r, Graph mst);
+int extractMin(Graph g);
 
 /* Main program */
 int main() {
@@ -33,6 +44,9 @@ int main() {
 	Graph no_airports;
 	Graph with_airports;
 
+	Graph mst_no_airports;
+	Graph mst_with_airports;
+
 	if (scanf("%d", &vertices) == -1) {
 		
 		perror("scanf\n");
@@ -41,6 +55,10 @@ int main() {
 	/* initializing a graph that uses airports and one that doesn't */
 	no_airports = initGraph(vertices);
 	with_airports = initGraph(vertices + 1);
+
+	/* initializing respective minimum spanning trees */
+	mst_no_airports = initGraph(vertices);
+	mst_with_airports = initGraph(vertices + 1);
 
 	if (scanf("%d", &airport_edges) == -1) {
 
@@ -73,6 +91,24 @@ int main() {
 		addEdge(with_airports, (road_src - 1), (road_dest - 1), road_cost);
 	}
 	
+	printf("No airports\n");
+
+	prim(no_airports, 0, mst_no_airports);
+
+		for (i = 0; i < no_airports->vertices; i++) {
+
+		printf("%d's parent is %d\n", (i+1), pi[i]+1);
+	}
+
+	printf("With airports\n");
+
+	prim(with_airports, 0, mst_with_airports);
+
+	for (i = 0; i < with_airports->vertices; i++) {
+
+		printf("%d's parent is %d\n", (i+1), pi[i]+1);
+	}
+
 	return 0;
 }
 
@@ -90,6 +126,10 @@ Graph initGraph(int vertices) {
 
 		graph->adjacency_list[i] = NULL;
 	}
+
+	key = (int *) malloc(vertices * sizeof(vertices));
+	pi = (int *) malloc(vertices * sizeof(vertices));
+	visited = (int *) malloc(vertices * sizeof(vertices));
 
 	return graph;
 }
@@ -113,4 +153,76 @@ Node addNode(int vertex, int cost, Node head) {
 	new->next = head;
 
 	return new;
+}
+
+void prim(Graph g, int r, Graph mst) {
+
+	int u;
+	int i;
+	int length;
+	Node head;
+	Node v;
+	
+	for (u = 0; u < g->vertices; u++) {
+
+		key[u] = INT_MAX;
+		pi[u] = NIL;
+		visited[u] = FALSE;
+	}
+
+	key[r] = 0;
+
+	length = g->vertices;
+
+	while (length != 0) {
+
+		u = extractMin(g);
+		length--;
+
+		head = g->adjacency_list[u];
+
+		for (; head != NULL; head = head->next) {
+
+			v = head;
+			
+			if ((visited[v->vertex] == FALSE) && (v->cost < key[v->vertex])) {
+
+				pi[v->vertex] = u;
+				addEdge(mst, u, v->vertex, v->cost);
+				
+				key[v->vertex] = v->cost;
+			} 
+		}
+	}
+
+	for (i = 0; i < g->vertices; i++) {
+			
+		if ((pi[i] == NIL) && (i != r)) {
+
+			printf("Insuficiente\n");
+			exit(0);
+		}
+	}
+}
+
+int extractMin(Graph g) {
+
+	int i;
+	int min_cost, min_cost_idx;
+
+	min_cost_idx = 0;
+	min_cost = INT_MAX;
+
+	for (i = 0; i < g->vertices; i++) {
+
+		if ((visited[i] == FALSE) && (key[i] < min_cost)) {
+
+			min_cost = key[i];
+			min_cost_idx = i;
+		}
+	}
+
+	visited[min_cost_idx] = TRUE;
+
+	return min_cost_idx;
 }
