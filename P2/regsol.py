37dc68f56c8a01e36bb7aceced3c63593bfd23d0
filipaBit <filipalpgouveia@gmail.com>
2017-11-26 15:52:35 +0000

    disse ao gajo para reformatar porque me estava a chatear os avisos
    fiz um esqueleto para o relatorio (no final ponho em colunas)

--- P2/regsol.py
+++ P2/regsol.py
@@ -4,19 +4,21 @@ from sklearn.kernel_ridge import KernelRidge
 from sklearn.model_selection import cross_val_score
 import timeit
 
-def mytraining(X,Y):
-    
-   
-    return reg
-    
-def mytrainingaux(X,Y,par):
-    
-    reg.fit(X,Y)
-                
+
+def mytraining(X, Y):
+
+    reg = KernelRidge(kernel="")
+    reg = reg.fit(X, Y)
     return reg
 
-def myprediction(X,reg):
 
+def mytrainingaux(X, Y, par):
+    reg.fit(X, Y)
+
+    return reg
+
+
+def myprediction(X, reg):
     Ypred = reg.predict(X)
 
     return Ypred
