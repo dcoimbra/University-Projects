65b3519de894ce3053f25a6bf5ad426eb69a9b07
Lúcia <lucy.hf.c@gmail.com>
2017-11-29 21:59:23 +0000

    Correção nas restrições do P1

--- sql/schema.sql
+++ sql/schema.sql
@@ -18,28 +18,34 @@ CREATE TABLE super_categoria (
 CREATE TABLE constituida (
   super_categoria varchar(20) REFERENCES super_categoria(nome),
 	categoria varchar(20) REFERENCES categoria(nome),
+  CHECK (super_categoria != categoria),
   PRIMARY KEY (super_categoria, categoria)
 );
 
 CREATE TABLE fornecedor (
-  nif numeric(9,0),
+  nif integer,
+  CHECK (nif > 8 and nif <= 9),
 	nome varchar(20),
 	PRIMARY KEY (nif)
 );
 
 CREATE TABLE produto (
-	ean numeric(13, 0),
+	ean integer,
+  CHECK (ean > 12 and ean <= 13),
 	design varchar(50),
   categoria varchar(20) REFERENCES categoria(nome),
-  forn_primario numeric(9,0) REFERENCES fornecedor(nif) NOT NULL,
+  forn_primario integer REFERENCES fornecedor(nif) NOT NULL,
+  CHECK (produto.forn_primario > 8 and produto.forn_primario <= 9),
   data date,
 	PRIMARY KEY (ean),
 	UNIQUE (design)
 );
 
 CREATE TABLE fornece_sec (
-  nif numeric(9, 0) REFERENCES fornecedor(nif) NOT NULL,
-  ean numeric(13, 0) REFERENCES produto(ean) ON DELETE CASCADE,
+  nif integer REFERENCES fornecedor(nif) NOT NULL,
+  CHECK (nif > 8 and ean <= 9),
+  ean integer REFERENCES produto(ean) ON DELETE CASCADE,
+  CHECK (ean > 12 and ean <= 13),
   PRIMARY KEY (nif, ean)
 );
 
@@ -58,7 +64,8 @@ CREATE TABLE prateleira (
 );
 
 CREATE TABLE planograma (
-  ean numeric(13, 0) REFERENCES produto(ean) ON DELETE CASCADE,
+  ean integer REFERENCES produto(ean) ON DELETE CASCADE,
+  CHECK (ean > 12 and ean <= 13),
   nro integer,
   lado varchar(10),
   CHECK (lado IN ('esquerdo', 'direito')),
@@ -79,7 +86,8 @@ CREATE TABLE evento_reposicao (
 );
 
 CREATE TABLE reposicao (
-  ean numeric(13, 0),
+  ean integer,
+  CHECK (ean > 12 and ean <= 13),
   nro integer,
   lado varchar(20),
   altura varchar(10),
