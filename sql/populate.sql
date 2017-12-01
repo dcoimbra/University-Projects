f4b52e1afff000310df7ba65db41cdc8587ceb5c
Coimbra <david.coimbra@tecnico.ulisboa.pt>
2017-12-01 18:55:57 +0000

    Adicionada categoria refrigerantes

--- sql/populate.sql
+++ sql/populate.sql
@@ -9,22 +9,24 @@ INSERT INTO categoria(nome) VALUES ('bebidas');
 INSERT INTO categoria(nome) VALUES ('bebidas com gas');
 INSERT INTO categoria(nome) VALUES ('sobremesas');
 INSERT INTO categoria(nome) VALUES ('cozinha');
+INSERT INTO categoria(nome) VALUES ('refrigerantes');
 
 INSERT INTO categoria_simples(nome) VALUES ('gelados');
 INSERT INTO categoria_simples(nome) VALUES ('frutos vermelhos');
 INSERT INTO categoria_simples(nome) VALUES ('peixe');
 INSERT INTO categoria_simples(nome) VALUES ('pao');
 INSERT INTO categoria_simples(nome) VALUES ('bolos');
-INSERT INTO categoria_simples(nome) VALUES ('bebidas com gas');
+INSERT INTO categoria_simples(nome) VALUES ('refrigerantes');
 
 INSERT INTO super_categoria(nome) VALUES ('fruta');
 INSERT INTO super_categoria(nome) VALUES ('congelados');
 INSERT INTO super_categoria(nome) VALUES ('bebidas');
 INSERT INTO super_categoria(nome) VALUES ('sobremesas');
 INSERT INTO super_categoria(nome) VALUES ('cozinha');
-
+INSERT INTO super_categoria(nome) VALUES ('bebidas com gas');
 
 INSERT INTO constituida(super_categoria, categoria) VALUES ('bebidas', 'bebidas com gas');
+INSERT INTO constituida(super_categoria, categoria) VALUES ('bebidas com gas', 'refrigerantes');
 INSERT INTO constituida(super_categoria, categoria) VALUES ('congelados', 'peixe');
 INSERT INTO constituida(super_categoria, categoria) VALUES ('cozinha', 'sobremesas');
 INSERT INTO constituida(super_categoria, categoria) VALUES ('sobremesas', 'fruta');
