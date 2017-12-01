8dc284ed4fc5a0bf2089705466a90d7b9add893e
filipaBit <filipalpgouveia@gmail.com>
2017-12-01 19:01:11 +0000

    pode ser assim? se  não não temos nada com varias sub-categorias para listar

--- sql/populate.sql
+++ sql/populate.sql
@@ -1,8 +1,10 @@
 INSERT INTO categoria(nome) VALUES ('gelados');
 INSERT INTO categoria(nome) VALUES ('frutos vermelhos');
 INSERT INTO categoria(nome) VALUES ('peixe');
+INSERT INTO categoria(nome) VALUES ('refrigerantes');
 INSERT INTO categoria(nome) VALUES ('pao');
 INSERT INTO categoria(nome) VALUES ('bolos');
+INSERT INTO categoria(nome) VALUES ('cervejas');
 INSERT INTO categoria(nome) VALUES ('fruta');
 INSERT INTO categoria(nome) VALUES ('congelados');
 INSERT INTO categoria(nome) VALUES ('bebidas');
@@ -13,18 +15,22 @@ INSERT INTO categoria(nome) VALUES ('cozinha');
 INSERT INTO categoria_simples(nome) VALUES ('gelados');
 INSERT INTO categoria_simples(nome) VALUES ('frutos vermelhos');
 INSERT INTO categoria_simples(nome) VALUES ('peixe');
+INSERT INTO categoria_simples(nome) VALUES ('refrigerantes');
 INSERT INTO categoria_simples(nome) VALUES ('pao');
 INSERT INTO categoria_simples(nome) VALUES ('bolos');
-INSERT INTO categoria_simples(nome) VALUES ('bebidas com gas');
+INSERT INTO categoria_simples(nome) VALUES ('cervejas');
 
 INSERT INTO super_categoria(nome) VALUES ('fruta');
 INSERT INTO super_categoria(nome) VALUES ('congelados');
 INSERT INTO super_categoria(nome) VALUES ('bebidas');
+INSERT INTO super_categoria(nome) VALUES ('bebidas com gas');
 INSERT INTO super_categoria(nome) VALUES ('sobremesas');
 INSERT INTO super_categoria(nome) VALUES ('cozinha');
 
 
 INSERT INTO constituida(super_categoria, categoria) VALUES ('bebidas', 'bebidas com gas');
+INSERT INTO constituida(super_categoria, categoria) VALUES ('bebidas com gas', 'refrigerantes');
+INSERT INTO constituida(super_categoria, categoria) VALUES ('bebidas com gas', 'cervejas');
 INSERT INTO constituida(super_categoria, categoria) VALUES ('congelados', 'peixe');
 INSERT INTO constituida(super_categoria, categoria) VALUES ('cozinha', 'sobremesas');
 INSERT INTO constituida(super_categoria, categoria) VALUES ('sobremesas', 'fruta');
