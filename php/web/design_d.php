b152c50e55f743016f8f2543f84ef9de461fb9c5
filipaBit <filipalpgouveia@gmail.com>
2017-12-02 16:03:50 +0000

    designação é agora required quando é modificada

--- php/web/design_d.php
+++ php/web/design_d.php
@@ -6,7 +6,7 @@
     <h3>Mudar designacao do produto <?=$_REQUEST['ean']?></h3>
     <form action="edit_design_d.php" method="post">
         <p><input type="hidden" name="ean" value="<?=$_REQUEST['ean']?>"/></p>
-        <p>Nova designacao: <input type="text" name="design" autocomplete="off"/></p>
+        <p>Nova designacao: <input type="text" name="design" autocomplete="off" required/></p>
         <p><input type="submit" value="Submit"/></p>
     </form>
     <?php
