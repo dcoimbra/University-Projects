6b2f9446e9c38a14e4fb25ffab20142075e49ab0
Lúcia <lucy.hf.c@gmail.com>
2017-12-14 00:53:53 +0000

    Adicionado populate do d_tempo, corrigido ROLLUP

--- sql/MM.sql
+++ sql/MM.sql
@@ -30,24 +30,56 @@ INSERT INTO d_produto (cean, categoria, nif_fornecedor_principal)
 SELECT ean, categoria, forn_primario
 FROM produto;
 
+-- -------------------------------------------------------------------------------------------------------------
+DO $$ BEGIN
+  PERFORM "polulate"();
+END $$;
+
+DROP FUNCTION IF EXISTS polulate();
+
+CREATE FUNCTION polulate()
+  returns void
+AS
+$$
+DECLARE date DATE := '2017-01-01';
+BEGIN
+  while date < '2018-12-31'
+  loop
+    INSERT INTO d_tempo (dia, mes, ano) VALUES (
+      extract(day from date),
+      extract(month from date),
+      extract(year from date)
+    );
+    date := date + INTERVAL '1 day';
+  end loop;
+END
+$$
+language plpgsql;
+
+-- -----------------------------------------------------------------------------------------------------------
+
 INSERT INTO facts_table(cean, dia, mes, ano, n_reposicoes)
 SELECT ean, extract(day from instante), extract (month from instante), extract (year from instante), unidades
 FROM reposicao;
 
+-- -----------------------------------------------------------------------------------------------------------------
 (SELECT count(n_reposicoes), categoria, mes, ano
   FROM facts_table, d_produto
-  WHERE facts_table.cean = d_produto.cean and nif_fornecedor_principal = '123 455 678'
-  GROUP BY mes, ano)
+  WHERE facts_table.cean = d_produto.cean and nif_fornecedor_principal = '123455678'
+  GROUP BY categoria, mes, ano)
 
 UNION
 
 (SELECT count(n_reposicoes), categoria, null, ano
   FROM facts_table, d_produto
-  WHERE facts_table.cean = d_produto.cean and nif_fornecedor_principal = '123 455 678'
-  GROUP BY ano)
+  WHERE facts_table.cean = d_produto.cean and nif_fornecedor_principal = '123455678'
+  GROUP BY categoria, ano)
 
 UNION
 
 (SELECT count(n_reposicoes), categoria, null, null
  FROM facts_table, d_produto
- WHERE facts_table.cean = d_produto.cean and nif_fornecedor_principal = '123 455 678');
+ WHERE facts_table.cean = d_produto.cean and nif_fornecedor_principal = '123455678'
+ GROUP BY categoria);
+
+-- ----------------------------------------------------------------------------------------------------------------
