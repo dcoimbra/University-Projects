c064fc370c40a10293a63faf521d3afff6d1b46f
NANS-DESKTOP\Lu <lucy.hf.c@gmail.com>
2017-12-19 22:00:32 +0000

    Última versão do schema.sql (novos triggers)

--- sql/schema.sql
+++ sql/schema.sql
@@ -95,22 +95,42 @@ CREATE TABLE reposicao (
   FOREIGN KEY (ean, nro, lado, altura) REFERENCES planograma(ean, nro, lado, altura) ON DELETE CASCADE
 );
 
-CREATE OR REPLACE FUNCTION check_forn() RETURNS TRIGGER
-  AS $$
-  BEGIN
+CREATE OR REPLACE FUNCTION check_forn_sec() RETURNS TRIGGER
+AS $$
+BEGIN
 
-    IF new.nif IN (SELECT forn_primario
-                   FROM produto
-                   WHERE new.nif = produto.forn_primario AND new.ean = produto.ean)
-      THEN RAISE EXCEPTION 'Fornecedor secundario escolhido ja e fornecedor primario deste produto';
-    END IF;
+  IF new.nif IN (SELECT forn_primario
+                 FROM produto
+                 WHERE new.nif = produto.forn_primario AND new.ean = produto.ean)
+  THEN RAISE EXCEPTION 'Fornecedor secundario escolhido ja e fornecedor primario deste produto';
+  END IF;
 
-    RETURN new;
-  END;
-  $$ LANGUAGE plpgsql;
+  RETURN new;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION check_forn_prim() RETURNS TRIGGER
+AS $$
+BEGIN
+
+  IF new.forn_primario IN (SELECT nif
+                           FROM fornece_sec
+                           WHERE new.forn_primario = fornece_sec.nif AND new.ean = fornece_sec.ean)
+  THEN RAISE EXCEPTION 'Fornecedor primario escolhido ja e fornecedor secundario deste produto';
+  END IF;
+
+  RETURN new;
+END;
+$$ LANGUAGE plpgsql;
 
 -- Antes de inserir o fornecedor secundario, verificar se nao e ja fornecedor primario desse produto
 CREATE TRIGGER check_forn_sec
-  BEFORE INSERT ON fornece_sec
+  BEFORE INSERT OR UPDATE OF nif ON fornece_sec
   FOR EACH ROW
-  EXECUTE PROCEDURE check_forn();
+EXECUTE PROCEDURE check_forn_sec();
+
+CREATE TRIGGER check_update_forn_prim
+  BEFORE UPDATE OF forn_primario ON produto
+  FOR EACH ROW
+EXECUTE PROCEDURE check_forn_prim();
