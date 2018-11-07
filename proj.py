6ea6806f97e6ce482a8efd2c0b5ed12b8f8d4022
Unknown <nuno.bombico@tecnico.ulisboa.pt>
2018-11-07 16:43:45 +0000

    create Json object

--- proj.py
+++ proj.py
@@ -1,11 +1,12 @@
 import sys
+import json
 
-def readFile(file):
+def readJson(file):
     print("Reading file %s" % file)
     f = open(file, 'r')
     file_contents = f.read()
     f.close()
-    return file_contents
+    return json.loads(file_contents)
 
 def usage(progName):
     print('Usage:')
@@ -16,4 +17,6 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         usage(sys.argv[0])
 
-    print(readFile(sys.argv[1]))
+    #print(readFile(sys.argv[1]))
+    
+    print(readJson(sys.argv[1]))
