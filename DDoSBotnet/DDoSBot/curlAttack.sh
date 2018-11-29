623d6bf662e6b8236f3e6abef80e9ba81acfc143
David Coimbra <david.coimbra@tecnico.ulisboa.pt>
2018-11-29 22:23:59 +0000

    ping & snort rules/conf

--- DDoSBotnet/DDoSBot/curlAttack.sh
+++ DDoSBotnet/DDoSBot/curlAttack.sh
@@ -2,7 +2,7 @@
 while true
 do
 #Please use 'man curl' to see what -vk is for, also -X command can be useful too.
-curl -sk easyappointments:8888 > /dev/null
+ping 172.29.0.4 > /dev/null
 #sleep 5
 done
 exit 0
